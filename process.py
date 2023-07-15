693cdf5ae919511998e9bb2e5d237ce34eea1057
Anita Graser <anitagraser@gmx.at>
2023-07-15 20:02:03 +0200

    Simplify

--- process.py
+++ process.py
@@ -9,20 +9,6 @@ from datetime import datetime, timedelta
 from os.path import exists
 from urllib.request import urlretrieve
 
-def get_osm_traces(page=0, bbox='16.18,48.09,16.61,48.32'):
-    file = 'osm_traces.gpx'
-    url = f'https://api.openstreetmap.org/api/0.6/trackpoints?bbox={bbox}&page={page}'
-    if not exists(file):
-        urlretrieve(url, file)
-    gdf = gpd.read_file(file, layer='track_points')
-    # dropping empty columns
-    gdf.drop(columns=['ele', 'course', 'speed', 'magvar', 'geoidheight', 'name', 'cmt', 'desc',
-       'src', 'url', 'urlname', 'sym', 'type', 'fix', 'sat', 'hdop', 'vdop',
-       'pdop', 'ageofdgpsdata', 'dgpsid'], inplace=True) 
-    gdf['t'] = pd.to_datetime(gdf['time'])
-    gdf.set_index('t', inplace=True)
-    return gdf
-
 
 with open("out.txt", "w") as outfile:
     outfile.write("Hello world! :-) \n")
@@ -50,12 +36,7 @@ ax = toy_traj.plot(column="speed", cmap="PRGn")
 point.plot(ax=ax, color='hotpink', markersize=100)
 plt.savefig("mpd-plot.png")
 
-
-gdf = get_osm_traces()
-osm_traces = mpd.TrajectoryCollection(gdf, 'track_fid')
-osm_traces = mpd.MinTimeDeltaGeneralizer(osm_traces).generalize(tolerance=timedelta(minutes=1))
-osm_traces.hvplot(title='OSM Traces', line_width=7, width=700, height=500)
-geoviews_plot = osm_traces.get_trajectory(0).hvplot(
+geoviews_plot = toy_traj.hvplot(
     title='Speed (m/s) along track', c='speed', cmap='RdYlBu',
     line_width=7, width=700, height=500, tiles='CartoLight', colorbar=True)
 gv.save(geoviews_plot, "mpd-hvplot.png")
